bb7c58f738be1dd90764e8495e82d23ff3db3ed7
Davi Ferreira Cordeiro <davizin5@hotmail.com>
2017-11-13 11:03:29 -0200

    Adicionado as funções de threads e a matriz op com suas devidas alterações

---
+++ CampoMinado/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int campo[10][10];
+
+int main()
+    {
+        int tam,escolha;
+        printf("########## BOAS VINDAS AO CAMPO MINADO ##########\n");
+
+    imprime_inicio();
+
+
+    return 1;
+}
+
